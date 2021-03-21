778e5c4218951532f1bbfb1867fe95eda97eafb3
Yaroslav Omelianenko <omelianenkoyarosllav@gmail.com>
2021-03-21 12:26:49 +0200

    registration page was done

---
+++ js/script.js
@@ -0,0 +1,62 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const passwordCheck = document.getElementById('passwordCheck');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs() {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const passwordCheckValue = passwordCheck.value.trim();
+
+    if (usernameValue === '') {
+        setErrorFor(username, 'Username cannot be blank');
+    } else {
+        setSuccessFor(username);
+    }
+    if (emailValue === '') {
+        setErrorFor(email, 'Email cannot be blank');
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Email is not valid');
+    } else {
+        setSuccessFor(email);
+    }
+    if(passwordValue ===''){
+        setErrorFor(password, 'Password cannot be blank');
+    } else {
+        setSuccessFor(password); 
+    }
+    if(passwordCheckValue ===''){
+        setErrorFor(passwordCheck, 'Password check cannot be blank');
+    }else if(passwordValue !== passwordCheckValue) {
+        setErrorFor(passwordCheck, 'Passwords does not mutch');
+    }else {
+        setSuccessFor(passwordCheck); 
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement; //.form-control
+    const small = formControl.querySelector('small');
+
+    small.innerText = message;
+
+    formControl.className = 'form-control error';
+}
+function setSuccessFor(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+function isEmail(email) {
+	return (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(email);
+}
+setCookie('user', 'John', {secure: true, 'max-age': 3600});
+document.cookie = "user=John; secure";
